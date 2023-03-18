b3ac45f958aa7d5415ec595a1dcd0b6a72db4513
maeddes <maeddes@gmail.com>
2023-03-18 18:45:45 +0000

    Revert "Spring Boot 2.7 and Otel"
    This reverts commit 8a5fc996399cd199892cbae6cc27dec1aa002b4c.

--- todobackend/src/test/java/io/novatec/todobackend/TodobackendApplicationIntegrationTests.java
+++ todobackend/src/test/java/io/novatec/todobackend/TodobackendApplicationIntegrationTests.java
@@ -1,12 +1,38 @@
 package io.novatec.todobackend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
 
-@SpringBootTest
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TodobackendApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TodobackendApplicationIntegrationTests {
 
+    public TodobackendApplicationIntegrationTests(){}
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TodobackendApplication todobackendApplication;
+
+    TestRestTemplate restTemplate = new TestRestTemplate();
+    
+    HttpHeaders headers = new HttpHeaders();
+
     @Test
-    void contextLoads() {
+    public void testGetTodos() {
+        todobackendApplication.addTodo("todo1");
+        todobackendApplication.addTodo("todo2");
+        todobackendApplication.addTodo("todo3");
+
+        assertEquals(3, todobackendApplication.getTodos().size());
     }
 }
