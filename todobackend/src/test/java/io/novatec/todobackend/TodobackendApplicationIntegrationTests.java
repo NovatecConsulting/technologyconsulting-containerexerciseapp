ca414fd8b841250f9567598a0548551c2296b88a
maeddes <maeddes@gmail.com>
2023-07-03 08:42:52 +0000

    Spring Boot 3 Java 17 JUnit 5

--- todobackend/src/test/java/io/novatec/todobackend/TodobackendApplicationIntegrationTests.java
+++ todobackend/src/test/java/io/novatec/todobackend/TodobackendApplicationIntegrationTests.java
@@ -1,17 +1,17 @@
 package io.novatec.todobackend;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpHeaders;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = TodobackendApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TodobackendApplicationIntegrationTests {
 
